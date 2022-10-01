1f1e04b9237150e7acd3e789036068708a29366a
YouYou94 <ybh4115@gmail.com>
2022-10-01 18:15:32 +0900

    pull request_01

--- MergeTest.js
+++ MergeTest.js
@@ -6,6 +6,10 @@ const BFunction = ({ a, b }) => {
   return a - b;
 };
 
+const CFunction = ({ a, b }) => {
+  return a * b;
+};
+
 const value_01 = 10;
 
 const value_02 = 12;
@@ -13,3 +17,5 @@ const value_02 = 12;
 console.log(`value_01 과 value_02의 합 : ${AFunction(value_01, value_02)}`);
 
 console.log(`value_01 과 value_02의 차 : ${BFunction(value_01, value_02)}`);
+
+console.log(`value_01 과 value_02의  곱 : ${CFunction(value_01, value_02)}`);
