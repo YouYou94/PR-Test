71b0e160cd463591a771d3e29a7d2878da27f3b7
YouYou94 <ybh4115@gmail.com>
2022-10-01 18:17:27 +0900

    MERGE SUCCESS!

--- MergeTest.js
+++ MergeTest.js
@@ -19,3 +19,5 @@ console.log(`value_01 과 value_02의 합 : ${AFunction(value_01, value_02)}`);
 console.log(`value_01 과 value_02의 차 : ${BFunction(value_01, value_02)}`);
 
 console.log(`value_01 과 value_02의  곱 : ${CFunction(value_01, value_02)}`);
+
+console.log(`MERGE SUCCESS!`);
